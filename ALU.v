5818c80a4930c872100a8b356725e483860138a8
Yashvi96011 <yashvibaraiya07@gmail.com>
2026-05-24 12:13:22 +0530

    Create ALU.v

---
+++ ALU.v
@@ -0,0 +1,23 @@
+module ALU (
+    input      [31:0] a,
+    input      [31:0] b,
+    input      [3:0]  alu_control,
+    output reg [31:0] result,
+    output            zero
+);
+
+always @(*) begin
+    case (alu_control)
+        4'b0000: result = a & b;
+        4'b0001: result = a | b;
+        4'b0010: result = a + b;
+        4'b0110: result = a - b;
+        4'b0111: result = (a < b) ? 32'b1 : 32'b0;
+        4'b1100: result = ~(a | b);
+        default: result = 32'b0;
+    endcase
+end
+
+assign zero = (result == 32'b0);
+
+endmodule
